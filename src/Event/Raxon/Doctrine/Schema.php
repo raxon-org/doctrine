7c0378c97bdf067a0b8b7831635d994a77906039
remco van der Velde <remco.pc@outlook.com>
2025-03-11 17:42:17 +0100

    init

--- src/Event/Raxon/Doctrine/Schema.php
+++ src/Event/Raxon/Doctrine/Schema.php
@@ -33,9 +33,7 @@ class Schema {
                     is_array($node->environment) ||
                     is_object($node->environment)
                 ){
-                    ddd($node->environment);
                     foreach($node->environment as $name => $environments){
-
                         foreach($environments as $environment => $config){
                             d($config);
                             $config->table = Table::all($object, $config->name, $config->environment);
