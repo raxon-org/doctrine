5bb26dca2bdf049ed48cbde30969fd5166c8c9e0
remco van der Velde <remco.pc@outlook.com>
2025-03-12 12:59:50 +0100

    init

--- src/Service/Schema.php
+++ src/Service/Schema.php
@@ -1399,7 +1399,7 @@ class Schema extends Main
                 throw new Exception('Platform not found, are you connected?');
             }
         }
-        ddd($node);
+        d($node);
         $schema = new \Doctrine\DBAL\Schema\Schema();
         $schema_table = $schema->createTable($node->get('table'));
         $columns = $node->get('column');
