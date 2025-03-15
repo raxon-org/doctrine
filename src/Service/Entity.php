d2cd371a78a76f76cd5083d1313da674708063d9
remco van der Velde <remco.pc@outlook.com>
2025-03-15 15:51:34 +0100

    added function namespace to get the namespace of a file

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -808,15 +808,9 @@ class Entity extends Main
             foreach($order as $key => $value){
                 $qb->orderBy($alias . '.' . $key, mb_strtoupper($value));
             }
-            d($order);
-            d($parameters);
-            d($firstResult);
             $qb->setParameters($parameters)
                 ->setFirstResult($firstResult)
                 ->setMaxResults($limit);
-            d($qb->getQuery()->getSQL());
-            d($alias);
-            ddd($options);
             $paginator = new Paginator($qb->getQuery(), $options['fetchJoinCollection']);
             $expose = Entity::expose_get(
                 $object,
