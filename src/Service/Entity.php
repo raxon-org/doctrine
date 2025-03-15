ec9656a62c229246f0c3b60cf2f3a029264f0985
remco van der Velde <remco.pc@outlook.com>
2025-03-15 16:26:23 +0100

    added function namespace to get the namespace of a file

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -669,9 +669,6 @@ class Entity extends Main
         }
         $pagination = $object->request('pagination');
         $filter = Entity::filter($object, $where, $parameters);
-        d($where);
-        d($parameters);
-        ddd($filter);
         $order = Core::object($object->request('order'), Core::OBJECT_ARRAY);
         $alias = lcfirst($entity);
         $data = [];
