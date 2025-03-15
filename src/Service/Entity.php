b304eb33d8261fd0d7a00d02b30aa92e061dafb2
remco van der Velde <remco.pc@outlook.com>
2025-03-15 17:25:16 +0100

    added function namespace to get the namespace of a file

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -77,6 +77,7 @@ class Entity extends Main
                         $class,
                         $request
                     );
+                    ddd($node);
                     try {
                         $entityManager->persist($node);
                         $entityManager->flush();
