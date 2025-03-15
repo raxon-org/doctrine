53875fc191ee76babbfaa659a2fcce0ddc7a176e
remco van der Velde <remco.pc@outlook.com>
2025-03-15 09:25:12 +0100

    added function namespace to get the namespace of a file

--- src/Service/Schema.php
+++ src/Service/Schema.php
@@ -121,12 +121,12 @@ class Schema extends Main
                         ){
                             $column_join = $column->options->join->column[0];
                             $data_columns[] = '#[ORM\OneToOne(';
-                            $data_columns[] = '    targetEntity: ' . $column->options->target;
+                            $data_columns[] = '    targetEntity: "' . $column->options->target .'",';
                             $data_columns[] = '    cascade: ["persist", "remove"]';
                             $data_columns[] = ')]';
                             $data_columns[] = '#[ORM\JoinColumn(';
-                            $data_columns[] = '    name: "' . $column_join->name . '"';
-                            $data_columns[] = '    referencedColumnName: "' . $column_join->reference . '"';
+                            $data_columns[] = '    name: "' . $column_join->name . '",';
+                            $data_columns[] = '    referencedColumnName: "' . $column_join->reference . '",';
                             $data_columns[] = '    nullable: ' . ($column_join->nullable === true ? 'true' : 'false');
                             $data_columns[] = ')]';
                         } else {
