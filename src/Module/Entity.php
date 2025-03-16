4f15e53cef9ab5b0699815e1942a994cf04382e4
remco van der Velde <remco.pc@outlook.com>
2025-03-16 09:34:32 +0100

    added function namespace to get the namespace of a file

---
+++ src/Module/Entity.php
@@ -0,0 +1,18 @@
+<?php
+namespace Raxon\Doctrine\Module;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+
+class Entity {
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public static function readById(App $object, EntityManager $em, string $entity, int $id){
+
+        return $em->find($entity, $id);
+    }
+}
