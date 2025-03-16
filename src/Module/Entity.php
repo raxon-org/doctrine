4217ef4320ec71a4b8e4f3cd6e212eccf4d6b783
remco van der Velde <remco.pc@outlook.com>
2025-03-16 10:56:24 +0100

    added function namespace to get the namespace of a file

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -23,8 +23,7 @@ class Entity {
      * @throws ORMException
      * @throws Exception
      */
-    public static function readById(App $object, EntityManager $em, mixed $role, string $entity, int $id){
-        ddd($role);
+    public static function readById(App $object, EntityManager $em, object $role, string $entity, int $id){
         $function = 'read';
         $node = $em->find($object->config('doctrine.entity.prefix') . $entity, $id);
         if($node) {
@@ -37,11 +36,11 @@ class Entity {
             );
             $record = Entity::expose(
                 $object,
+                $role,
                 $node,
                 $toArray,
                 $entity,
                 $function,
-                $record
             );
             $data['node'] = $record;
             return $data;
@@ -49,7 +48,7 @@ class Entity {
         throw new Exception('Cannot find entity: ' . $entity .', with id: ' . $id);
     }
 
-    public static function readByUuid(App $object, EntityManager $em, string $entity, string $uuid): array
+    public static function readByUuid(App $object, EntityManager $em, object $role, string $entity, string $uuid): array
     {
         $function = 'read';
         $repository = $em->getRepository($object->config('doctrine.entity.prefix') . $entity);
@@ -66,11 +65,11 @@ class Entity {
             );
             $record = Entity::expose(
                 $object,
+                $role,
                 $node,
                 $toArray,
                 $entity,
                 $function,
-                $record
             );
             $data['node'] = $record;
             return $data;
@@ -83,141 +82,86 @@ class Entity {
      * @throws Exception
      * @throws AuthorizationException
      */
-    public static function expose(App $object, $node, $expose=[], $class='', $function='', $internalRole=false, $parentRole=false): Data
+    public static function expose(App $object, $role, $node, $expose=[], $class='', $function=''): Data
     {
         if (!is_array($expose)) {
             return new Data();
         }
-        $roles = [];
-        if ($internalRole) {
-            $roles[] = $internalRole; //same as parent
-        } else {
-//            $roles = Permission::getAccessControl($object, $class, $function);
-            try {
-                /*
-                $user = User::getByAuthorization($object);
-                if ($user) {
-                    $roles = $user->getRolesByRank('asc');
-                }
-                */
-            } catch (Exception $exception) {
-
-            }
-        }
-        if (empty($roles)) {
-            throw new Exception('Roles failed...');
-        }
         $record = [];
         $is_expose = false;
-        foreach ($roles as $role) {
-            $permissions = $role->permission;
-            if (is_array($permissions)) {
-                foreach ($permissions as $permission) {
-                    foreach ($expose as $action) {
+        $permissions = $role->permission;
+        if (is_array($permissions)) {
+            foreach ($permissions as $permission) {
+                foreach ($expose as $action) {
+                    if (
+                        (
+                            $permission->name === str_replace('.', ':', Controller::name($class)) . ':' . str_replace('_', '.', $function) &&
+                            property_exists($action, 'role') &&
+                            $action->role === $role->name
+                        )
+                        ||
+                        (
+                            in_array(
+                                $function,
+                                ['child', 'children'],
+                                true
+                            ) &&
+                            property_exists($action, 'role') &&
+                            $action->role === $role->name
+                        )
+                    ) {
+                        $is_expose = true;
                         if (
-                            (
-                                $permission->name === str_replace('.', ':', Controller::name($class)) . ':' . str_replace('_', '.', $function) &&
-                                property_exists($action, 'role') &&
-                                $action->role === $role->name
-                            )
-                            ||
-                            (
-                                in_array(
-                                    $function,
-                                    ['child', 'children'],
-                                    true
-                                ) &&
-                                property_exists($action, 'role') &&
-                                $action->role === $parentRole
-                            )
+                            property_exists($action, 'property') &&
+                            is_array($action->property)
                         ) {
-                            $is_expose = true;
-                            if (
-                                property_exists($action, 'property') &&
-                                is_array($action->property)
-                            ) {
 
-                                foreach ($action->property as $property) {
-                                    $is_optional = false;
-                                    if(substr($property, 0, 1) === '?'){
-                                        $is_optional = true;
-                                        $property = substr($property, 1);
-                                    }
-                                    $assertion = $property;
-                                    $explode = explode(':', $property, 2);
-                                    $compare = null;
-                                    $method = 'get' . ucfirst($explode[0]);
-                                    $value = $node->{$method}();
-                                    if (array_key_exists(1, $explode)) {
-                                        $record_property = $value;
-                                        $compare = $explode[1];
-                                        $attribute = $explode[0];
-                                        if ($compare) {
-                                            $parse = new Parse($object, $object->data());
-                                            $compare = $parse->compile($compare, $object->data());
-                                            if ($record_property !== $compare) {
-                                                throw new Exception('Assertion failed: ' . $assertion . ' values [' . $record_property . ', ' . $compare . ']');
-                                            }
+                            foreach ($action->property as $property) {
+                                $is_optional = false;
+                                if(substr($property, 0, 1) === '?'){
+                                    $is_optional = true;
+                                    $property = substr($property, 1);
+                                }
+                                $assertion = $property;
+                                $explode = explode(':', $property, 2);
+                                $compare = null;
+                                $method = 'get' . ucfirst($explode[0]);
+                                $value = $node->{$method}();
+                                if (array_key_exists(1, $explode)) {
+                                    $record_property = $value;
+                                    $compare = $explode[1];
+                                    $attribute = $explode[0];
+                                    if ($compare) {
+                                        $parse = new Parse($object, $object->data());
+                                        $compare = $parse->compile($compare, $object->data());
+                                        if ($record_property !== $compare) {
+                                            throw new Exception('Assertion failed: ' . $assertion . ' values [' . $record_property . ', ' . $compare . ']');
                                         }
                                     }
+                                }
+                                if (
+                                    property_exists($action, 'object') &&
+                                    property_exists($action->object, $property) &&
+                                    property_exists($action->object->$property, 'expose')
+                                ) {
                                     if (
-                                        property_exists($action, 'object') &&
-                                        property_exists($action->object, $property) &&
-                                        property_exists($action->object->$property, 'expose')
+                                        property_exists($action->object->$property, 'multiple') &&
+                                        $action->object->$property->multiple === true &&
+                                        $value !== null
                                     ) {
-                                        if (
-                                            property_exists($action->object->$property, 'multiple') &&
-                                            $action->object->$property->multiple === true &&
-                                            $value !== null
-                                        ) {
-                                            $array = $value;
+                                        $array = $value;
 
-                                            if(is_array($array) || is_object($array)){
-                                                $record[$property] = [];
-                                                foreach ($array as $child) {
-                                                    $child = new Data($child);
-                                                    $child_expose =[];
-                                                    if(
-                                                        property_exists($action->object->$property, 'object')
-                                                    ){
-                                                        $child_expose[] = (object) [
-                                                            'property' => $action->object->$property->expose,
-                                                            'object' => $action->object->$property->object,
-                                                            'role' => $action->role,
-                                                        ];
-                                                    }  else {
-                                                        $child_expose[] = (object) [
-                                                            'property' => $action->object->$property->expose,
-                                                            'role' => $action->role,
-                                                        ];
-                                                    }
-                                                    $child = Entity::expose(
-                                                        $object,
-                                                        $child,
-                                                        $child_expose,
-                                                        $property,
-                                                        'child',
-                                                        $role,
-                                                        $action->role
-                                                    );
-                                                    $record[$property][] = $child->data();
-                                                }
-                                            } else {
-                                                //leave intact for read without parse
-                                                $record[$property] = $array;
-                                            }
-                                        } elseif ($value !== null) {
-                                            $child = $value;
-                                            if (!empty($child)) {
-                                                $record[$property] = null;
+                                        if(is_array($array) || is_object($array)){
+                                            $record[$property] = [];
+                                            foreach ($array as $child) {
                                                 $child = new Data($child);
                                                 $child_expose =[];
                                                 if(
-                                                    property_exists($action->object->$property, 'objects')
+                                                    property_exists($action->object->$property, 'object')
                                                 ){
                                                     $child_expose[] = (object) [
                                                         'property' => $action->object->$property->expose,
-                                                        'object' => $action->object->$property->objects,
+                                                        'object' => $action->object->$property->object,
                                                         'role' => $action->role,
                                                     ];
                                                 }  else {
@@ -228,34 +172,67 @@ class Entity {
                                                 }
                                                 $child = Entity::expose(
                                                     $object,
+                                                    $role,
                                                     $child,
                                                     $child_expose,
                                                     $property,
-                                                    'child',
-                                                    $role,
-                                                    $action->role
+                                                    'child'
                                                 );
-                                                $record[$property] = $child->data();
+                                                $record[$property][] = $child->data();
                                             }
-                                            if (empty($record[$property])) {
-                                                $record[$property] = null;
-                                            }
+                                        } else {
+                                            //leave intact for read without parse
+                                            $record[$property] = $array;
                                         }
-                                    } else {
-                                        if ($value !== null) {
-                                            $record[$property] = $value;
+                                    } elseif ($value !== null) {
+                                        $child = $value;
+                                        if (!empty($child)) {
+                                            $record[$property] = null;
+                                            $child = new Data($child);
+                                            $child_expose =[];
+                                            if(
+                                                property_exists($action->object->$property, 'objects')
+                                            ){
+                                                $child_expose[] = (object) [
+                                                    'property' => $action->object->$property->expose,
+                                                    'object' => $action->object->$property->objects,
+                                                    'role' => $action->role,
+                                                ];
+                                            }  else {
+                                                $child_expose[] = (object) [
+                                                    'property' => $action->object->$property->expose,
+                                                    'role' => $action->role,
+                                                ];
+                                            }
+                                            $child = Entity::expose(
+                                                $object,
+                                                $role,
+                                                $child,
+                                                $child_expose,
+                                                $property,
+                                                'child',
+                                            );
+                                            $record[$property] = $child->data();
+                                        }
+                                        if (empty($record[$property])) {
+                                            $record[$property] = null;
                                         }
                                     }
+                                } else {
+                                    if ($value !== null) {
+                                        $record[$property] = $value;
+                                    }
                                 }
-                                if(!empty($record)){
-                                    break 3;
-                                }
+                            }
+                            if(!empty($record)){
+                                break 2;
                             }
                         }
                     }
                 }
             }
         }
+
         if($is_expose === false){
             throw new Exception('No permission found for ' . str_replace('.', ':', Controller::name($class)) . ':' . str_replace('_', '.', $function));
         }
