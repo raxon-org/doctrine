ff67357bf8c41d04d25693b4663cb8424ff9dd7b
remco van der Velde <remco.pc@outlook.com>
2025-03-16 14:52:46 +0100

    added function namespace to get the namespace of a file

--- src/Event/Raxon/Doctrine/Schema.php
+++ src/Event/Raxon/Doctrine/Schema.php
@@ -34,7 +34,9 @@ class Schema {
                     foreach($node->environment as $name => $environments){
                         foreach($environments as $environment => $connection){
                             $em = Database::entity_manager($object, $config, $connection);
-                            $connection->table = $em->listTableNames();
+                            $em_connection = $em->getConnection();
+                            $schema_manager = $em_connection->createSchemaManager();
+                            $connection->table = $schema_manager->listTables();
                             ddd($connection);
                             if(in_array($node->table, $config->table, true)){
                                 /**
