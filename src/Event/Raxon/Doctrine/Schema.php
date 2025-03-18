acb1ae7f96ebb5cf5e925c0370087ed276aaaba9
remco van der Velde <remco.pc@outlook.com>
2025-03-18 16:44:06 +0100

    sqlite

--- src/Event/Raxon/Doctrine/Schema.php
+++ src/Event/Raxon/Doctrine/Schema.php
@@ -26,6 +26,7 @@ class Schema {
         $is_entity = false;
         $is_repository = false;
         $config = Database::config($object);
+        ddd($options);
         if(array_key_exists('node', $options)){
             $node = $options['node'];
             if(property_exists($node, 'environment')){
