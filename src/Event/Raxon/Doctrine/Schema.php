43add3c6857f7c2451e64fbb16a01bbc23cc98cb
remco van der Velde <remco.pc@outlook.com>
2025-03-18 16:37:42 +0100

    sqlite

--- src/Event/Raxon/Doctrine/Schema.php
+++ src/Event/Raxon/Doctrine/Schema.php
@@ -42,6 +42,7 @@ class Schema {
                             $schema_manager = $em_connection->createSchemaManager();
                             $connection->table = $schema_manager->listTableNames();
                             */
+                            ddd($connection);
                             if(in_array($node->table, $config->table, true)){
                                 /**
                                  * rename goes wrong (we need to rename to much like the indexes uniques)
