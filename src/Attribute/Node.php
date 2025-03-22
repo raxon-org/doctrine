8dc91d13f06b1ec89a832280d62fd44a0f1a128f
remco van der Velde <remco.pc@outlook.com>
2025-03-22 21:28:30 +0100

    sqlite

--- src/Attribute/Node.php
+++ src/Attribute/Node.php
@@ -1,10 +1,17 @@
 <?php
 namespace Raxon\Doctrine\Attribute;
 
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+
 class Node
 {
     public function __construct(
         public readonly string $class,
+        public readonly array $filter=[],
+        public readonly array $where=[],
+        public readonly array $sort=[],
     ) {
 
     }
