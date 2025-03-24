0523bfcb0a3a9c57ae715002616bf1919f839bdd
remco van der Velde <remco.pc@outlook.com>
2025-03-24 09:01:11 +0100

    sqlite

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -682,8 +682,8 @@ class Entity {
                                             $compare = $explode[1];
                                             $attribute = $explode[0];
                                             if ($compare) {
-                                                $object->config('parse.cast', 'string');
                                                 $parse = new Parse($object, $object->data());
+                                                $object->config('parse.cast', 'string');
                                                 $compare = $parse->compile($compare, $object->data());
                                                 //add reflection to check if it has a node chain.
                                                 d($method);
