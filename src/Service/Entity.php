d576820599458028fc84c0481f4fb9ba79103ba4
remco van der Velde <remco.pc@outlook.com>
2025-03-25 19:31:44 +0100

    öö

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -791,7 +791,6 @@ class Entity extends Main
             $data['count'] = (int) $count;
             $data['page'] = $page;
             $data['limit'] = $limit;
-            ddd($data);
             $qb = $entityManager->createQueryBuilder();
             $qb->select([$alias])
                 ->from($entityName, $alias);
