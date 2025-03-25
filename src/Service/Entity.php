af8946bfe2906b5eef7c028d6091d97b0c3d72db
remco van der Velde <remco.pc@outlook.com>
2025-03-25 20:38:01 +0100

    öö

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -664,10 +664,6 @@ class Entity extends Main
         }
         $pagination = $object->request('pagination');
         $filter = Entity::filter($object, $where, $parameters);
-        d($object->request());
-        d($filter);
-        d($parameters);
-        d($where);
         $order = Core::object($object->request('order'), Core::OBJECT_ARRAY);
         $alias = lcfirst($entity);
         $data = [];
