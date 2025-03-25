1c623b02fae3a5399da020999a73150b5d489acf
remco van der Velde <remco.pc@outlook.com>
2025-03-25 16:08:02 +0100

    sqlite account/application

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -454,9 +454,9 @@ trait Main {
             $connection = $object->config('doctrine.environment.' . $options->connection . '.' . '*');
         }
         $em = Database::entity_manager($object, $config, $connection);
-        $backup_connection = clone $connection;
-        $backup_connection->path = str_replace('System.db', 'System.Backup.db', $backup_connection->path);
-        $em = Database::entity_manager($object, $config, $backup_connection);
+//        $backup_connection = clone $connection;
+//        $backup_connection->path = str_replace('System.db', 'System.Backup.db', $backup_connection->path);
+//        $em = Database::entity_manager($object, $config, $backup_connection);
         $connection_em = $em->getConnection();
         $data = [];
         if(File::exist($options->url)){
