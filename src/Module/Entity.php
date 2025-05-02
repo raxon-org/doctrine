b66983f51331d42ca13accc3a6025581b1491ad9
remco van der Velde <remco.pc@outlook.com>
2025-05-02 17:52:47 +0200

    implementing new parser

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -238,6 +238,7 @@ class Entity {
         foreach ($data as $node) {
             if(File::exist($validate_url)) {
                 $data_node = new Data($node);
+                trace();
                 d($data_node);
                 $validate = Entity::validate($object, $validation, $data_node->data());
                 ddd($validate);
