c5ff326c20160aac386d3baeb1d9cc77a1e79be5
remco van der Velde <remco.pc@outlook.com>
2025-05-02 15:40:14 +0200

    implementing new parser

--- src/Module/Schema.php
+++ src/Module/Schema.php
@@ -17,6 +17,7 @@ class Schema{
 
     public static function entity(App $object, $class, $role, $node, $options=[]): void
     {
+        d($node);
         if (is_object($node)) {
             $node_class = get_class($node);
             switch ($node_class) {
@@ -31,6 +32,7 @@ class Schema{
         } else {
             return;
         }
+        d($node);
         if ($node->has('table')) {
             $table = $node->get('table');
             $entity = $node->get('entity');
