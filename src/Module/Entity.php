12d50cd49f69ace4ae15ee29bf4266afa46ce4cc
remco van der Velde <remco.pc@outlook.com>
2025-05-28 20:25:07 +0200

    init

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -199,6 +199,7 @@ class Entity {
     {
         $data = $object->data(sha1($url));
         if($data === null){
+            d($url);
             $data = $object->parse_read($url, sha1($url));
         }
         if($data){
