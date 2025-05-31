5b61aa861d8ef168e6dab2e9761b7263b0ccf609
remco van der Velde <remco.pc@outlook.com>
2025-05-31 14:32:33 +0200

    init

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -761,7 +761,7 @@ class Entity {
                                                     $array = $response['list'] ?? [];
                                                 }
                                                 foreach ($array as $child) {
-                                                    if(property_exists($child, '#class')){
+                                                    if(property_exists($child, '#class') || Core::is_uuid($child)){
                                                         $child_record = $child;
                                                     } else {
                                                         //need to add the node output if its a node (has property #class)
