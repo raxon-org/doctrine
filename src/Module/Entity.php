0a2dff7b056d82024823b88d9969daaf20251186
remco van der Velde <remco.pc@outlook.com>
2025-06-22 03:06:24 +0200

    php 8.4 deprecations

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -1007,6 +1007,7 @@ class Entity {
             $data['nodeList'] = [];
             $qb = $entityManager->createQueryBuilder();
             $entityName = $object->config('doctrine.entity.prefix') . $entity;
+            ddd($entityName);
             $joins = Entity::get_joins($object, $entity);
             $qb->select(['count(' . $alias . '.id)'])
                 ->from($entityName, $alias);
