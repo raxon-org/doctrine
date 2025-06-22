10ff7d9c57f2a8b50ba32124abaf385da2a32746
remco van der Velde <remco.pc@outlook.com>
2025-06-22 02:41:31 +0200

    php 8.4 deprecations

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -663,6 +663,8 @@ class Entity extends Main
             $options->fetchJoinCollection = true;
         }
         $pagination = $object->request('pagination');
+        trace();
+        ddd($object->request());
         $filter = Entity::filter($object, $where, $parameters);
         $order = Core::object($object->request('order'), Core::OBJECT_ARRAY);
         $alias = lcfirst($entity);
