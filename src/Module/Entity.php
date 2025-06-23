df9545bf2768c6c089773e333968e7391fe6d3fc
remco van der Velde <remco.pc@outlook.com>
2025-06-23 17:55:12 +0200

    php 8.4 deprecations

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -1267,8 +1267,6 @@ class Entity {
      * @throws \ReflectionException
      */
     private static function filter(App $object, &$where=[], ArrayCollection|null &$parameters=null){
-        d($_SERVER);
-        ddd($object->request());
         $request = $object->request('filter') ?? [];
         $alias = lcfirst($object->request('entity'));
         $filter = $request ?? [];
