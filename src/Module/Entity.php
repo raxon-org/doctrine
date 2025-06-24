83f1a42f1c0321a708b7765d42c9ad7701295b1e
remco van der Velde <remco.pc@outlook.com>
2025-06-24 11:51:37 +0200

    php 8.4 deprecations

--- src/Module/Entity.php
+++ src/Module/Entity.php
@@ -695,7 +695,6 @@ class Entity {
                                     property_exists($action, 'property') &&
                                     is_array($action->property)
                                 ) {
-                                    d($action);
                                     foreach ($action->property as $attribute) {
                                         $assertion = $attribute;
                                         $explode = explode(':', $attribute, 2);
