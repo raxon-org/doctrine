4ed87e2a07a894a6ee1ea1276e706bacc7537aca
Remco van der Velde <remco@universeorange.com>
2026-01-17 14:39:50 +0100

    make raxon/doctrine default for sqlite user database

---
+++ src/Package/Trait/Setup.php
@@ -0,0 +1,189 @@
+<?php
+namespace Package\Raxon\Doctrine\Trait;
+
+use Exception;
+use Raxon\App;
+use Raxon\Config;
+use Raxon\Exception\DirectoryCreateException;
+use Raxon\Exception\FileWriteException;
+use Raxon\Exception\ObjectException;
+use Raxon\Module\Core;
+use Raxon\Module\File;
+use Raxon\Module\Dir;
+use Raxon\Node\Module\Node;
+
+trait Setup {
+
+    /**
+     * @throws DirectoryCreateException
+     * @throws ObjectException
+     * @throws FileWriteException
+     */
+    public function role_system_import(): void
+    {
+        $object = $this->object();
+        $package = $object->request('package');
+        if($package){
+            $node = new Node($object);
+            $node->role_system_create($package);
+        }
+    }
+
+     /**
+     * @throws Exception
+     */
+    public function register(): bool
+    {
+        $object = $this->object();
+        $status = false;
+        $options = App::options($object);
+        $node = new Node($object);
+        $record_options = [
+            'where' => [
+                [
+                    'value' => $object->request('package'),
+                    'attribute' => 'name',
+                    'operator' => '===',
+                ]
+            ]
+        ];
+        $class = 'System.Installation';
+        $response = $node->record($class, $node->role_system(), $record_options);
+        if(
+            $response &&
+            array_key_exists('node', $response)
+        ){
+            if(property_exists($options, 'force')){
+                $record = $response['node'];
+                $record->mtime = time();
+                $response = $node->put($class, $node->role_system(), $record);
+                echo 'Register update ' . $object->request('package') . ' installation...' . PHP_EOL;
+                $status = true;
+            }
+            elseif(property_exists($options, 'patch')){
+                $record = $response['node'];
+                $record->mtime = time();
+                $response = $node->patch($class, $node->role_system(), $record);
+                echo 'Register update ' . $object->request('package') . ' installation...' . PHP_EOL;
+                $status = true;
+            }
+            else {
+                echo 'Skipping ' . $object->request('package') . ' installation...' . PHP_EOL;
+            }
+        } else {
+            $time = time();
+            $record = (object) [
+                'name' => $object->request('package'),
+                'ctime' => $time,
+                'mtime' => $time,
+            ];
+            $response = $node->create($class, $node->role_system(), $record);
+            echo 'Registering ' . $object->request('package') . ' installation...' . PHP_EOL;
+            $status = true;
+        }
+        return $status;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function doctrine_bin(object $flags, object $options): void
+    {
+        $object = $this->object();
+        $posix_id = $object->config(Config::POSIX_ID);
+        if(
+            !in_array(
+                $posix_id,
+                [
+                    0,
+                ],
+                true
+            )
+        ){
+            throw new Exception('Access denied...');
+        }
+        $url_bin = $object->config('project.dir.vendor') . 'raxon/doctrine/src/Bin/Doctrine.php';
+        $url_target = $object->config('project.dir.binary') . 'Doctrine.php';
+        File::copy($url_bin, $url_target);
+        $url_bin_source = $object->config('project.dir.vendor') . 'raxon/doctrine/src/Bin/doctrine';
+        $url_bin_target = '/usr/bin/doctrine';
+        File::copy($url_bin_source, $url_bin_target);
+        $command = 'chmod +x ' . $url_bin_target;
+        exec($command);
+        File::permission($object, [
+            'url_target' => $url_target,
+        ]);
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    public function system_config(object $flags, object $options): void
+    {
+        $object = $this->object();
+        $posix_id = $object->config(Config::POSIX_ID);
+        if(
+            !in_array(
+                $posix_id,
+                [
+                    0,
+                    33
+                ],
+                true
+            )
+        ){
+            throw new Exception('Access denied...');
+        }
+        $node = new Node($object);
+        $config = $node->record('System.Config', $node->role_system());
+        if(
+            $config &&
+            is_array($config) &&
+            array_key_exists('node', $config) &&
+            property_exists($config['node'], 'uuid') // &&
+//            !property_exists($config['node'], 'doctrine')
+        ){
+            $patch = (object) [
+                'uuid' => $config['node']->uuid,
+                'doctrine' => '*'
+            ];
+            $config = $node->patch('System.Config', $node->role_system(), $patch);
+            if(
+                is_array($config) &&
+                array_key_exists('node', $config)
+            ){
+                //nothing
+            } else {
+                throw new Exception('Could not patch node System.Config');
+            }
+        }
+    }
+
+    /**
+     * @throws ObjectException
+     * @throws Exception
+     */
+    public function system_doctrine(object $flags, object $options): void
+    {
+        $object = $this->object();
+        $posix_id = $object->config(Config::POSIX_ID);
+        if (
+            !in_array(
+                $posix_id,
+                [
+                    0,
+                    33
+                ],
+                true
+            )
+        ) {
+            throw new Exception('Access denied...');
+        }
+        $node = new Node($object);
+        $class = 'System.Doctrine';
+        $config = $node->record($class, $node->role_system());
+        ddd($config);
+    }
+
+}
--- src/Package/View/Setup/Setup.tpl
+++ src/Package/View/Setup/Setup.tpl
@@ -1,7 +1,7 @@
-{{$register = Package.Raxon.Doctrine:Init:register()}}
+{{$register = Package.Raxon.Doctrine:Setup:register()}}
 {{if(!is.empty($register))}}
-{{Package.Raxon.Doctrine:Import:role.system()}}
-{{$options = options()}}
-{{Package.Raxon.Doctrine:Main:system.config($options)}}
-{{Package.Raxon.Doctrine:Main:bin.doctrine($options)}}
+{{Package.Raxon.Doctrine:Setup:role.system.import()}}
+{{$response = Package.Raxon.Doctrine:Setup:system.config(flags(), options())}}
+{{$response = Package.Raxon.Doctrine:Setup:system.doctrine(flags(), options())}}
+{{$response = Package.Raxon.Doctrine:Setup:doctrine.bin(flags(), options())}}
 {{/if}}
