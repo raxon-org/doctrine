9b889e674f1fec80df5e604c5d1635a12c1e63ee
Remco van der Velde <remco@universeorange.com>
2026-01-24 19:15:06 +0100

    pp

--- src/Package/Trait/Setup.php
+++ src/Package/Trait/Setup.php
@@ -412,7 +412,7 @@ trait Setup {
                 $connection->manager = Database::entity_manager($object, $config, $connection);
                 $connection->schema_manager = Database::schema_manager($connection->manager);
                 $tables = $connection->schema_manager->listTableNames();
-                d($tables);
+                breakpoint($tables);
                 foreach($list as $file) {
                     if ($file->type === File::TYPE && property_exists($file, 'data')) {
                         if (!empty($file->data)) {
