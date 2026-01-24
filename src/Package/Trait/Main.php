c4b57dbef1b14e2ea2c1d391182308493f95ad2c
Remco van der Velde <remco@universeorange.com>
2026-01-24 20:44:28 +0100

    pp

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -451,9 +451,7 @@ trait Main {
                 $pop = substr(array_pop($data), 0 ,-1);
                 $data[] = $pop;
                 $data[] = '}';
-
-
-                echo implode('', $data) . PHP_EOL;
+                echo implode(PHP_EOL, $data) . PHP_EOL;
                 foreach($list['nodeList'] as $row){
                     $data = [];
                     $data[] = '{';
@@ -463,7 +461,7 @@ trait Main {
                     $pop = substr(array_pop($data), 0 ,-1);
                     $data[] = $pop;
                     $data[] = '}';
-                    echo implode('', $data) . PHP_EOL;
+                    echo implode(PHP_EOL, $data) . PHP_EOL;
                 }
             }
         }
