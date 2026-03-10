cd2d4345879eaed34b9bdee6462983f5c20a8311
Remco van der Velde <remco@universeorange.com>
2026-03-10 14:38:50 +0100

    init

--- src/Service/Entity.php
+++ src/Service/Entity.php
@@ -819,6 +819,7 @@ class Entity extends Main
                 $entity,
                 $entity . '.'. $options->function . '.output'
             );
+            breakpoint($expose);
             foreach ($paginator as $node) {
                 $record = [];
                 $record = Entity::output(
