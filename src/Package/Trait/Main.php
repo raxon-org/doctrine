057184d29b033b8d7be38f632f0be932a7cdc75c
Remco van der Velde <remco@universeorange.com>
2026-06-03 19:02:23 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -414,7 +414,7 @@ trait Main {
         }
         $object = $this->object();
         if(property_exists($options, 'limit')){
-            $object->reequest('limit',  (int) $options->limit);
+            $object->request('limit',  (int) $options->limit);
         }
         if(property_exists($options, 'page')){
             $object->request('page', (int) $options->page);
