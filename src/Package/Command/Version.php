2035a3fcc9df3ee0339fcbe2dfeb0fd028bf14b6
Remco van der Velde <remco@universeorange.com>
2026-06-03 14:48:33 +0200

    init

--- src/Package/Command/Version.php
+++ src/Package/Command/Version.php
@@ -1,10 +1,21 @@
 <?php
 namespace Package\Raxon\Doctrine\Command;
 
+use Composer\InstalledVersions;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 class Version extends \Symfony\Component\Console\Command\Command {
 
     public function __construct(null|string $name=null)
     {
         parent::__construct($name);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        echo 'symfony/console: ' . InstalledVersions::getVersion('symfony/console') . PHP_EOL;
+        echo 'doctrine/orm: ' .InstalledVersions::getVersion('doctrine/orm') . PHP_EOL;
+        echo 'doctrine/dbal: ' .InstalledVersions::getVersion('doctrine/dbal') . PHP_EOL;
     }
 }
